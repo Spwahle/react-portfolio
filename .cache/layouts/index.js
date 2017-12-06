2f2dfb05183b537a79df931094c4bca906da9afb
Shaun Wahle <shaun.wahle@gmail.com>
2017-12-05 21:05:04 -0800

    adds updated gitignore

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/shaun/react-portfolio/src/layouts/index.js"
+  import data from "/Users/shaun/react-portfolio/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
